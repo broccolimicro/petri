0759b8bcd57f5f682f6a34e021eac2aff36a2bc5
Ned Bingham <edward.bingham@broccolimicro.io>
2024-12-19 13:22:46 -0500

    adding another test for select

--- tests/select.cpp
+++ tests/select.cpp
@@ -357,3 +357,130 @@ TEST(select, nonproper_parallel) {
 	EXPECT_EQ(lst({{p[0],p[2]},{p[0],p[5]},{p[3],p[5]}}), g.select(choice, {p[0], p[2], p[3], p[5]}, true, true));
 }
 
+/*
+
+TODO(edward.bingham) this breaks the flat split group comparison method, may
+require recursive algorithms.
+
+TEST(select, shared_parallel) {
+	//          ->p1-->t1-->p2-->t2-->p3             .
+	//         /                        \            .
+	//     ->t0                          ->t5        .
+	//    /    \                        /    \       .
+	//  p0      ->p4-->t3-->p5-->t4-->p6      ->p10  .
+	//    \    /                        \    /       .
+	//     ->t6                          ->t9        .
+	//         \                        /            .
+	//          ->p7-->t7-->p8-->t8-->p9             .
+
+	graph<place, transition, token, state<token> > g;
+
+	auto p = g.create(place(), 11);
+	auto t = g.create(transition(), 10);
+
+	g.connect({p[0], t[0], p[1], t[1], p[2], t[2], p[3], t[5], p[10]});
+	g.connect({t[0], p[4], t[3], p[5], t[4], p[6], t[5]});
+	g.connect({p[0], t[6], p[4]});
+	g.connect({p[6], t[9], p[10]});
+	g.connect({t[6], p[7], t[7], p[8], t[8], p[9], t[9]});
+
+	g.compute_split_groups(parallel);
+	g.compute_split_groups(choice);
+
+}
+
+TEST(select, shared_choice) {
+	//          ->t1-->p1-->t2-->p2-->t3             .
+	//         /                        \            .
+	//     ->p0                          ->p5        .
+	//    /    \                        /    \       .
+	//  t0      ->t4-->p3-->t5-->p4-->t6      ->t10  .
+	//    \    /                        \    /       .
+	//     ->p6                          ->p9        .
+	//         \                        /            .
+	//          ->t7-->p7-->t8-->p8-->t9             .
+
+	graph<place, transition, token, state<token> > g;
+
+	auto p = g.create(place(), 10);
+	auto t = g.create(transition(), 11);
+
+	g.connect({t[0], p[0], t[1], p[1], t[2], p[2], t[3], p[5], t[10]});
+	g.connect({p[0], t[4], p[3], t[5], p[4], t[6], p[5]});
+	g.connect({t[0], p[6], t[4]});
+	g.connect({t[6], p[9], t[10]});
+	g.connect({p[6], t[7], p[7], t[8], p[8], t[9], p[9]});
+
+	g.compute_split_groups(parallel);
+	g.compute_split_groups(choice);
+
+}*/
+
+TEST(select, compressed_choice_parallel) {
+	//           ->p1-->t1--          .
+	//          /           \         .
+	//      ->t0             p2       .
+	//     /    \           /  \      .
+	//    /      ->p3-->t2 /    \     .
+	//  p0                X      >t6  .
+	//    \      ->p4-->t3 \    /     .
+	//     \    /           \  /      .
+	//      ->t4             p5       .
+	//          \           /         .
+	//           ->p6-->t5--          .
+
+	graph<place, transition, token, state<token> > g;
+
+	auto p = g.create(place(), 7);
+	auto t = g.create(transition(), 7);
+
+	g.connect({p[0], t[0], p[1], t[1], p[2], t[6]});
+	g.connect({t[0], p[3], t[2], p[5]});
+	g.connect({t[4], p[4], t[3], p[2]});
+	g.connect({p[0], t[4], p[6], t[5], p[5], t[6]});
+
+	g.compute_split_groups(parallel);
+	g.compute_split_groups(choice);
+
+	EXPECT_EQ(lst({{t[1],t[2]},{t[3],t[5]}}), g.select(parallel, {t[1], t[2], t[3], t[5]}, false, false));
+	EXPECT_EQ(lst({{t[1],t[2]},{t[3],t[5]}}), g.select(parallel, {t[1], t[2], t[3], t[5]}, false, true));
+	EXPECT_EQ(lst({{t[1],t[2]},{t[3],t[5]}}), g.select(parallel, {t[1], t[2], t[3], t[5]}, true, false));
+	EXPECT_EQ(lst({{t[1],t[2]},{t[3],t[5]}}), g.select(parallel, {t[1], t[2], t[3], t[5]}, true, true));
+	EXPECT_EQ(lst({{t[1],t[3]},{t[2],t[3]},{t[1],t[5]},{t[2],t[5]}}), g.select(choice, {t[1], t[2], t[3], t[5]}, false, false));
+	EXPECT_EQ(lst({{t[1],t[3]},{t[2],t[3]},{t[1],t[5]},{t[2],t[5]}}), g.select(choice, {t[1], t[2], t[3], t[5]}, false, true));
+	EXPECT_EQ(lst({{t[1],t[3]},{t[2],t[3]},{t[1],t[5]},{t[2],t[5]}}), g.select(choice, {t[1], t[2], t[3], t[5]}, true, false));
+	EXPECT_EQ(lst({{t[1],t[3]},{t[2],t[3]},{t[1],t[5]},{t[2],t[5]}}), g.select(choice, {t[1], t[2], t[3], t[5]}, true, true));
+
+}
+
+/* This structure violates liveness
+
+TEST(select, compressed_parallel_choice) {
+	// Without guards, this would deadlock when t3 and t4 or t1 and t6 are
+	// executed in parallel.
+	//           ->t1-->p1--          .
+	//          /           \         .
+	//      ->p0             t2       .
+	//     /    \           /  \      .
+	//    /      ->t3-->p2 /    \     .
+	//  t0                X      >p6  .
+	//    \      ->t4-->p3 \    /     .
+	//     \    /           \  /      .
+	//      ->p4             t5       .
+	//          \           /         .
+	//           ->t6-->p5--          .
+
+	graph<place, transition, token, state<token> > g;
+
+	auto p = g.create(place(), 7);
+	auto t = g.create(transition(), 7);
+
+	g.connect({t[0], p[0], t[1], p[1], t[2], p[6]});
+	g.connect({p[0], t[3], p[2], t[5]});
+	g.connect({p[4], t[4], p[3], t[2]});
+	g.connect({t[0], p[4], t[6], p[5], t[5], p[6]});
+
+	g.compute_split_groups(parallel);
+	g.compute_split_groups(choice);
+
+}*/
